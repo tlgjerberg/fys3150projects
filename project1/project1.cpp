25b605bc281e30367f49201645dc4f830726c649
tlgjerberg <torgje@hotmail.com>
2018-08-28 17:20:21 +0200

    Wrote functions for creating empty matrices and vectors

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int d = 
+int main()
+{
+
+}
+
+
+}
+
+
+double **emptymatrix(int row, int col)
+{
+  double **A;
+  A = new double*[row];
+  for (i = 0; i < row; i++){
+    A[i] = new double[col]
+  }
+  return (double **)A
+}
+
+double **emptyvector(int n)
+{
+ double *v;
+ v = new double[n];
+
+
+
+ return (double *)v
+}
