17387c0b1d07c50b3296c401ac5f3ac6114cc680
tlgjerberg <torgje@hotmail.com>
2018-08-31 14:51:22 +0200

    Declared vectors in main

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
-int main()
+double ** CreateMatrix(int row, int col);
+
+double *CreateVector(int n);
+
+int main(int argc , char* argv[])
 {
+int n = 4;
+double **A;
+A = CreateMatrix(n, n);
+  for (int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      A[i][j] = (j*3)/3;
+    }
 
-}
-
-
-}
-
-
-double **emptymatrix(int row, int col)
-{
-  double **A;
-  A = new double*[row];
-  for (i = 0; i < row; i++){
-    A[i] = new double[col]
   }
-  return (double **)A
+
+return 0;
 }
 
-double **emptyvector(int n)
+
+double **CreateMatrix(int row, int col)
+{
+  double **M;
+  M = new double*[row];
+  for (int i = 0; i < row; i++){
+    M[i] = new double[col];
+  }
+
+  }
+  return (double **)M;
+}
+
+double *CreateVector(int n)
 {
  double *v;
  v = new double[n];
 
-
-
- return (double *)v
+ return (double *)v;
 }
