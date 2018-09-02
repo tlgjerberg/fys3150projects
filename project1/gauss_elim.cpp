2f54dc45e24b5a25016e9dcb295127a0afd5cad9
tlgjerberg <torgje@hotmail.com>
2018-09-02 21:17:39 +0200

    Fixed mathematical error in computation of d and b arrays

--- project1/gauss_elim.cpp
+++ project1/gauss_elim.cpp
@@ -18,24 +18,27 @@ double *c = new double[n]; double *x = new double[n+1];
 
 
 
-double d_arg[n+1] = {2, 5, 7, 6};
-double a_arg[n] = {4, 5, 7};
-double c_arg[n] = {2, 9, 3};
+double d_arg[n] = {2, 5, 7, 6};
+double a_arg[n-1] = {4, 5, 7};
+double c_arg[n-1] = {2, 9, 3};
 
 x[0] = 0; b[0] = 1; a[0] = 1; c[0] = 1; d[0] = d_arg[0];
 
-for (int i = 1; i < n+1; i++){
+for (int i = 1; i < n; i++){
   x[i] = x[i-1]+h;
   b[i] = hh*f(x[i]);
 }
 
 for (int i = 1; i < n+1; i++){
-  d[i] = d_arg[i] - a[i-1]/d[i-1];
-  b[i] = b[i] - b[i-1]/d[i-1];
+  d[i] = d_arg[i] - (a[i-1]*c[i-1])/d[i-1];
+  b[i] = b[i] - (b[i-1]*e[i-1])/d[i-1];
 }
 
 double *u = new double[n+1];
-for (int j = n; j > 0; j++){
+
+u[n] = b[n]/d[n];
+
+for (int j = n-1; j > 0; j++){
   ;
 }
 
