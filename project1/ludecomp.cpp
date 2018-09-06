63ae0b9315f0690340a90848b3f9ddc13a42eb5b
tlgjerberg <torgje@hotmail.com>
2018-09-06 16:52:09 +0200

    Wrote lu-decompostion with armadillo

---
+++ project1/ludecomp.cpp
@@ -0,0 +1,69 @@
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <iomanip>
+#include <armadillo>
+using namespace std;
+using namespace arma;
+
+inline double f( double x){return 100.0*exp(-10*x);} //Analytical function f(x)
+
+//Closed form solution of f
+inline double closed_form(double x){return 1 - (1 - exp(-10))*x - exp(-10*x);}
+
+int main(int argc, char* argv[])
+{
+
+int n = atoi(argv[1]);
+double h = 1.0/double (n+1);
+double hh = h*h;
+
+double *x = new double[n]; //Declare array for x to be used in function f
+x[0] = h; x[n] = 1;
+double *f_cf = new double[n];
+
+vec b = zeros(n);
+
+
+for (int i = 1; i < n; i++)
+{
+  x[i] = i*h;
+  b[i] = hh*f(x[i]);
+  f_cf[i] = closed_form(x[i]);
+}
+
+mat A = zeros(n, n);
+A.diag() += 2;
+A.diag(1) += -1;
+A.diag(-1) += -1;
+
+cout << b;
+cout << A;
+
+clock_t c_start = clock(); //CPU time clock start
+
+vec v = solve(A, b);
+
+clock_t c_end = clock(); //CPU time clock stop
+
+cout << fixed << setprecision(2) << "CPU time used: "
+      << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n";
+
+// Printing x, numerical and analytical to file
+ofstream ofile;
+
+char *sol_array;
+
+ofile.open("sol_array.txt");
+
+for (int i = 0; i < n; i++)
+{
+  ofile << x[i] << " " << v[i] << " " << f_cf[i] << endl;
+}
+
+ofile.close();
+
+return 0;
+}
