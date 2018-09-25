2f42a87d91e11789514b107e9dc420e773cda6be
tlgjerberg <torgje@hotmail.com>
2018-09-25 12:50:38 +0200

    seperate maxoffdiag function

---
+++ project2/maxoffdiag.cpp
@@ -0,0 +1,23 @@
+#include "maxoffdiag.h"
+#include <armadillo>
+#include <cmath>
+#include <ctime>
+#include <iostream>
+
+using namespace std;
+using namespace arma;
+
+double maxoffdiag(mat A, int &k, int &l, int n) {
+  double max = 0.0;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      double a_ij = fabs(A(i, j));
+      if (a_ij > max) {
+        max = a_ij;
+        k = i;
+        l = j;
+      }
+    }
+  }
+  return max;
+}
