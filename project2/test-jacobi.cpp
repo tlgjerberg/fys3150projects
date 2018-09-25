3fdf0200f410898038665ec87a345c1304cfde17
tlgjerberg <torgje@hotmail.com>
2018-09-25 16:29:50 +0200

    Wrote test for checking the maximum off-diagonal element

--- project2/test-jacobi.cpp
+++ project2/test-jacobi.cpp
@@ -1,8 +1,25 @@
-#include "catch.hpp"
 #include "jacobi.h"
+#include "maxoffdiag.h"
+#include "toeplitz.h"
+#include <armadillo>
+// #include <catch2/catch.hpp>
+#include "catch.hpp"
+#include <cmath>
+#include <iostream>
 
-TEST_CASE("Testing the maximum offdiagonal element") {
-  int n = 3;
+using namespace std;
+using namespace arma;
+
+TEST_CASE("Testing the maximum off-diagonal element") {
+  int k, l;
+  int n = 45;
   double h = 1 / n;
   double hh = h * h;
+  mat A = rand_toeplitz(n, hh);
+  mat B = rand_toeplitz(n, hh);
+  B = abs(B);
+  B.diag().fill(0);
+  double max_B = B.max();
+  double max_A = maxoffdiag(A, k, l, n);
+  REQUIRE(max_A == Approx(max_B));
 }
