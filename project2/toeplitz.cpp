71e8afdfb491ad5d11534f263d2b27f9bab1dde5
tlgjerberg <torgje@hotmail.com>
2018-09-26 10:32:44 +0200

    Added function for a toeplitz matrix with a HO potential

--- project2/toeplitz.cpp
+++ project2/toeplitz.cpp
@@ -17,3 +17,15 @@ mat rand_toeplitz(int n, double hh) {
 
   return A;
 }
+
+// Toeplitz matrix for a harmonic oscillator potential
+mat HO_toeplitz(int n, double hh, vec rho) {
+  mat A = zeros(n, n);
+  // A.diag() += 2 / hh;
+  for (int i = 1; i < n; i++) {
+    A.diag()[i] += 2 / hh + rho[i];
+  }
+  A.diag(1) += -1 / hh;
+  A.diag(-1) += -1 / hh;
+  return A;
+}
