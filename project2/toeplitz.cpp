91aa6919b9b6b483d931c6ae17541bf479eac0b9
tlgjerberg <torgje@hotmail.com>
2018-09-27 16:03:12 +0200

    added function for two electron matrix

--- project2/toeplitz.cpp
+++ project2/toeplitz.cpp
@@ -3,6 +3,7 @@
 mat toeplitz(int n, double hh) {
   mat A = zeros(n, n);
   double d = 2 / hh;
+  double s_d = 1 / hh;
   A.diag() += d;
   A.diag(1) += -1 / hh;
   A.diag(-1) += -1 / hh;
@@ -23,9 +24,22 @@ mat rand_toeplitz(int n, double hh) {
 mat HO_toeplitz(int n, double hh, vec rho) {
   mat A = zeros(n, n);
   double d = 2 / hh;
+  double s_d = 1 / hh;
   A.diag() += (d + (rho % rho));
-  A.diag(1) += -1 / hh;
-  A.diag(-1) += -1 / hh;
+  A.diag(1) += -s_d;
+  A.diag(-1) += -s_d;
+  // A(0, 0) = 0;
+  // A(n - 1, n - 1) = 0;
+  return A;
+}
+
+mat HO_toeplitz_two_e(int n, double hh, vec rho, double omega_r) {
+  mat A = zeros(n, n);
+  double d = 2 / hh;
+  double s_d = 1 / hh;
+  A.diag() += (d + pow(omega_r, 2) * (rho % rho) + 1.0 / rho);
+  A.diag(1) += -s_d;
+  A.diag(-1) += -s_d;
   // A(0, 0) = 0;
   // A(n - 1, n - 1) = 0;
   return A;
