b0b9d7935edbbbc8b968852709e8be27d4631431
tlgjerberg <torgje@hotmail.com>
2018-09-28 10:56:05 +0200

    Moved jacobi_method function into jacobi.cpp

--- project2/jacobi.cpp
+++ project2/jacobi.cpp
@@ -47,3 +47,17 @@ void rotation(mat &A, mat &R, int &k, int &l, int n) {
   }
   return;
 }
+
+vec jacobi_method(mat A, mat &R, double eps, double max, int k, int l, int n) {
+  int max_iter = (double)n * (double)n * (double)n;
+  int iter = 0;
+  while (max > eps && iter < max_iter) {
+    max = maxoffdiag(A, k, l, n);
+    rotation(A, R, k, l, n);
+    iter++;
+  }
+  cout << "Number of iterations: " << iter << endl;
+  vec eigvals = sort(diagvec(A));
+  cout << R << endl;
+  return eigvals;
+}
