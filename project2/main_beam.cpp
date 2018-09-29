a0ab323619cb6c8729a727c66995d928024e9f66
tlgjerberg <torgje@lavabit.com>
2018-09-29 16:16:06 +0200

    Added analytical eigenvalues function and printing to a file

--- project2/main_beam.cpp
+++ project2/main_beam.cpp
@@ -1,5 +1,16 @@
 #include "jacobi.h"
 
+vec analytic_eigenvalues_buckling_beam(int n, double hh) {
+  double d = 2 / hh;
+  double a = -1 / hh;
+  double pi = M_PI;
+  vec lambda = zeros(n);
+  for (int j = 1; j < n - 1; j++) {
+    lambda[j] = d + 2 * a * cos((j * pi) / (n + 1));
+  }
+  return lambda;
+}
+
 int main(int argc, char const *argv[]) {
 
   int n = atoi(argv[1]);
@@ -15,8 +26,6 @@ int main(int argc, char const *argv[]) {
 
   eig_sym(eigval, eigvec, A);
 
-  cout << eigval << eigvec << endl;
-
   mat R = zeros(n, n);
   R.diag() += 1;
 
@@ -29,8 +38,24 @@ int main(int argc, char const *argv[]) {
 
   clock_t c_end = clock();
 
-  cout << jacobi_eigval << endl;
   cout << fixed << "CPU time used with Jacobi's method: "
        << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n";
+
+  vec analytic_eigval = analytic_eigenvalues_buckling_beam(n, hh);
+
+  ofstream ofile;
+
+  ofile.open("eigenvalues.txt");
+
+  ofile << "analytic_eigval"
+        << " "
+        << "jacobi_eigval" << endl;
+  for (int i = 0; i < n; i++) {
+    ofile << analytic_eigval[i] << " " << jacobi_eigval[i] << " " << endl;
+    // ofile << rho << " ";
+    // ofile << R(span::all, 0) << endl;
+  }
+  ofile.close();
+
   return 0;
 }
