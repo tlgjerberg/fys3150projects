9ea3714e3281f555a313c8f16596dce0e39b1a82
tlgjerberg <torgje@hotmail.com>
2018-10-01 10:17:34 +0200

    correct pull

--- project2/plot_e_optional.py
+++ project2/plot_e_optional.py
@@ -2,10 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-for i in ["eigenvectors_e_opt_0_05.txt", "eigenvectors_e_opt_0_5.txt", "eigenvectors_e_opt_1.txt", "eigenvectors_e_opt_5.txt", "eigenvectors_d.txt"]:
+for i in ["eigenvectors_e_opt_0.05", ]
     rho = []
     eigenvec = []
     with open(i, "r") as file:
+        next(file)
         for line in file:
             col = line.split()
             rho.append(float(col[0]))
@@ -16,11 +17,10 @@ for i in ["eigenvectors_e_opt_0_05.txt", "eigenvectors_e_opt_0_5.txt", "eigenvec
 
     plt.style.use('ggplot')
     fig, ax = plt.subplots()
-    ax.plot(rho_array, eigenvec_array, label='$\\omega_r = $')
 
+    ax.plot(rho_array, eigenvec_array, label='$\\omega_r = $')
     plt.legend(loc='best')
     ax.set_title("Ground state of a harmonic oscillator potential")
     ax.set_xlabel("$\\rho$")
     ax.set_ylabel("u($\\rho$)")
-
 plt.show()
