a0659d0916c9a6bb2cb460b798084df584753a09
tlgjerberg <torgje@lavabit.com>
2018-10-01 22:20:07 +0200

    Added test for eigenvalues

--- project2/test-jacobi.cpp
+++ project2/test-jacobi.cpp
@@ -22,8 +22,23 @@ TEST_CASE("Testing the maximum off-diagonal element") {
   REQUIRE(max_A == Approx(max_B));
 }
 
-TEST_CASE("Testing orthogonality of the eigenvectors") {
+TEST_CASE("Check if eigenvalues are correct") {
   int k, l;
-  int n = 10;
+  int n = 45;
   double h = 1 / n;
+  double hh = h * h;
+  double eps = 1e-8;
+  vec arma_eigval;
+  mat arma_eigvec;
+  eig_sym(arma_eigval, arma_eigvec, A);
+  mat A = rand_toeplitz(n, hh);
+  mat R = zeros(n, n);
+  R.diag() += 1;
+  double max = maxoffdiag(A, k, l, n);
+  vec eig_jac = jacobi_method(A, R, eps, max, k, l,
+                              n); // Find eigenvalues using Jacobi's method
+  vec eig_arma = arma_eigval;     // Generate eigenvalue vector with Armadillo
+  for (int i = 0; i < m; i++) {
+    REQUIRE(eig_jac(i) == Approx(eig_arma(i)));
+  }
 }
