a2f35b3c7d9ef787bc4eb581d34a6d5fa6d9c0fd
tlgjerberg <torgje@lavabit.com>
2018-10-09 18:28:07 +0200

    Added class Solar_system

---
+++ project3/solar_system.cpp
@@ -0,0 +1,18 @@
+#include "solar_system.h"
+
+class Solar_system {
+private:
+  double MS = 2e30;
+  double ME = 6e24;
+  double G = 6.67e-11;
+};
+
+int main(int argc, char const *argv[]) {
+  int n = 100;
+  double h = 0.01;
+  double *x = new double[n];
+  double *y = new double[n];
+  double *vx = new double[n];
+  double *vy = new double[n];
+  return 0;
+}
