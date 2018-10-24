1521207a857afb9d0e731e1173c5640645f0fb39
tlgjerberg <torgje@hotmail.com>
2018-10-24 22:01:30 +0200

    Removed unused code

--- project3/solar_system.cpp
+++ project3/solar_system.cpp
@@ -18,45 +18,6 @@ void SolarSystem::add_celestial_objects(planet planet) {
 }
 int SolarSystem::length() { return celestial_objects.size(); }
 
-// mat SolarSystem::position() {
-//   // int n = celestial_objects.size();
-//   mat pos = zeros(3, n);
-//   for (int i = 0; i < n; i++) {
-//     pos(0, i) = celestial_objects[i].x;
-//     pos(1, i) = celestial_objects[i].y;
-//     pos(2, i) = celestial_objects[i].z;
-//   }
-//   return pos;
-// }
-
-// mat SolarSystem::velocity() {
-//   int n = celestial_objects.size();
-//   mat vel = zeros(3, n);
-//   for (int i = 0; i < n; i++) {
-//     vel(0, i) = celestial_objects[i].vx;
-//     vel(1, i) = celestial_objects[i].vy;
-//     vel(2, i) = celestial_objects[i].vz;
-//   }
-//   return vel;
-// }
-
-// mat SolarSystem::Forces() {
-//   int n = celestial_objects.size();
-//   mat F = zeros(3, n);
-//   for (int i = 0; i < n; i++) {
-//     planet object1 = &celestial_objects[i];
-//     for (int j = i + 1; j < n; j++) {
-//       planet object2 = &celestial_objects[j];
-//
-//       vec Force1 = object1.GForce(object2);
-//       F(0, i) += Force1(0);
-//       F(1, i) += Force1(1);
-//       F(2, i) += Force1(2);
-//     }
-//   }
-//   return F
-// }
-
 /*Method calculating the acceleration on one object from every other object
 in the celestial_objects vector. Too save some steps the negative value */
 mat SolarSystem::accel() {
