edb275d95a90b41fa63d49aed5c8ca64ba1acff3
tlgjerberg <torgje@hotmail.com>
2018-11-09 13:18:49 +0100

    updated with ms19937 RNG

--- project4/Ising.cpp
+++ project4/Ising.cpp
@@ -59,11 +59,13 @@ map<double, double> transitions(double T) {
 
 // Flipps a random spin according to acceptence requirements
 void tryflip(mat &spin, int n, int Delta_E, map<double, double> W, int rx,
-             int ry, double &E, double &M) {
-  if (rand() % 2 <= W.find(Delta_E)->second) {
+             int ry, double &E, double &M, mt19937 &generator) {
+  uniform_int_distribution<int> dist(0, 1);
+  double r = dist(generator);
+  if (r <= W.find(Delta_E)->second) {
     spin(rx, ry) *= -1;
     cout << "Flipp" << endl;
-    M += (double)spin(rx, ry);
+    M += (double)2 * spin(rx, ry);
     E += (double)Delta_E;
   } else {
     cout << "No flipp" << endl;
@@ -72,7 +74,8 @@ void tryflip(mat &spin, int n, int Delta_E, map<double, double> W, int rx,
 
 // Metropolis algorithm
 void Metropolis(mat &spin, double T, int n, map<double, double> W, double &E,
-                double &M) {
+                double &M, mt19937 &generator) {
+
   double J = 1;
   for (int x = 0; x < n; x++) {
     for (int y = 0; y < n; y++) {
@@ -81,7 +84,7 @@ void Metropolis(mat &spin, double T, int n, map<double, double> W, double &E,
       int Delta_E = 2 * J * spin(rx, ry) *
                     (spin(rx, PBC(ry, n, 1)) + spin(PBC(ry, n, 1)) +
                      spin(rx, PBC(ry, n, -1)) + spin(PBC(ry, n, -1)));
-      tryflip(spin, n, Delta_E, W, rx, ry, E, M);
+      tryflip(spin, n, Delta_E, W, rx, ry, E, M, generator);
     }
   }
   return;
@@ -89,36 +92,29 @@ void Metropolis(mat &spin, double T, int n, map<double, double> W, double &E,
 
 void writebinary(double &E) {
   streampos size;
-  // char *memblock;
   ofstream outfile;
-  outfile.open("mean_energy.bin", ios::out | ios::app | ios::binary);
-  // outfile.seekp(0, ios::beg);
+  outfile.open("mean_energy.dat", ios::out | ios::app | ios::binary);
   outfile.write((char *)&E, sizeof(double));
-  // delete[] memblock;
   outfile.close();
   return;
 }
 
 int main(int argc, char const *argv[]) {
+  int temp_start;
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   // srand(time(NULL));
   mt19937 generator(seed);
-  // int r = gen_random();
-  // cout << r << endl;
+
   int L = 2;
   mat spin = zeros(L, L);
   double E, M;
   double T = 2.0;
   E = 0;
   M = 0;
-  vec w(5);
   initialize(spin, L, T, E, M);
-  // cout << spin << endl;
-  // flip_one(spin, 0, 1);
-  // cout << spin << endl;
   map<double, double> W = transitions(T);
-  cout << W.find(4)->second << endl;
-  Metropolis(spin, T, L, W, E, M);
+  // cout << W.find(4)->second << endl;
+  Metropolis(spin, T, L, W, E, M, generator);
   cout << E << endl;
   cout << M << endl;
   writebinary(E);
