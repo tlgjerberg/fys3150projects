7b61a1431d280135862dc1e3fc14ed9cdbf77f57
tlgjerberg <torgje@hotmail.com>
2018-11-09 16:57:24 +0100

    Fixed reading binary files with numpy

--- project4/read_bin.py
+++ project4/read_bin.py
@@ -1,5 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
-with open("mean_energy.bin", "rb") as binary_file:
+with open("mean_energy.dat", "rb") as binary_file:
     # Read the whole file at once
     data = binary_file.read()
-    print(data)
+    # print(data)
+
+file = "mean_energy.dat"
+array = np.fromfile(file, dtype="int32", count=-1)
+
+# print(array)
+
+mcs = np.linspace(0, len(array), len(array))
+
+plt.plot(mcs, array)
+plt.show()
