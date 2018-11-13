4290d64ffa442f48b57f91a712360c37822cbac3
tlgjerberg <torgje@hotmail.com>
2018-11-13 19:53:35 +0100

    Changed program to only print when one process is executed at a time and printing mean energy

--- project4/Ising.cpp
+++ project4/Ising.cpp
@@ -137,6 +137,7 @@ int main(int argc, char *argv[]) {
   double T = 100.0;
 
   ofstream outfile;
+
   initialize(spin, L, E, M, GS);
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
@@ -162,34 +163,37 @@ int main(int argc, char *argv[]) {
   // for (int T = initial_temp; T < final_temp; T += temp_step) {
 
   MC(spin, T, L, E, M, mcs, GS, averages);
-  if (my_rank == 0) {
-    outfilename2 = "mean_energy.txt";
-    ofstream efile;
-    efile.open(outfilename2);
-    MPI_Status status;
-    outfile.open(outfilename, ios::binary);
-    outfile.write(reinterpret_cast<const char *>(averages), mcs * sizeof(int));
+  if (numprocs == 1) {
+    if (my_rank == 0) {
 
-    for (int i = 1; i < numprocs; i++) {
-      MPI_Recv(averages, mcs, MPI_INT, MPI_ANY_SOURCE, 500, MPI_COMM_WORLD,
-               &status);
+      MPI_Status status;
+      outfile.open(outfilename, ios::binary);
       outfile.write(reinterpret_cast<const char *>(averages),
                     mcs * sizeof(int));
-    }
 
-    outfile.close();
-  } else {
-    MPI_Send(averages, mcs, MPI_INT, 0, 500, MPI_COMM_WORLD);
-    // MPI_Finalize();
+      for (int i = 1; i < numprocs; i++) {
+        MPI_Recv(averages, mcs, MPI_INT, MPI_ANY_SOURCE, 500, MPI_COMM_WORLD,
+                 &status);
+        outfile.write(reinterpret_cast<const char *>(averages),
+                      mcs * sizeof(int));
+      }
+
+      outfile.close();
+    } else {
+      MPI_Send(averages, mcs, MPI_INT, 0, 500, MPI_COMM_WORLD);
+      // MPI_Finalize();
+    }
   }
 
   MPI_Reduce(&E, &total_E, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
-  if (rank == 0) {
-
-    efile << total_E;
-    efile.close();
-  }
+  // if (rank == 0) {
+  outfilename2 = "mean_energy.txt";
+  ofstream efile;
+  efile.open(outfilename2);
+  efile << total_E;
+  efile.close();
+  // }
   // delete averages;
   // delete[] averages;
   // }
