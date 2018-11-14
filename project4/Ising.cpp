99b55d6b855456673002d44ac3f80c4da836ae01
tlgjerberg <torgje@lavabit.com>
2018-11-14 12:04:28 +0100

    finished seperating program for readability and cleaning up

--- project4/Ising.cpp
+++ project4/Ising.cpp
@@ -1,16 +1,4 @@
-#include "mpi.h"
-#include <armadillo>
-#include <chrono>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <random>
-
-using namespace std;
-using namespace arma;
+#include "Ising.h"
 
 // Gives a random number either -1 or 1
 int gen_random() {
@@ -38,7 +26,7 @@ void initialize(mat &spin, int n, double &E, double &M, int GS) {
       }
     }
   }
-
+  // Initial energy and magentic momentum
   for (int x = 0; x < n; x++) {
     for (int y = 0; y < n; y++) {
 
@@ -72,7 +60,7 @@ void tryflip(mat &spin, int n, int Delta_E, map<double, double> W, int rx,
              int ry, double &E, double &M, mt19937 &generator) {
 
   uniform_real_distribution<float> dist(0, 1);
-  double r = dist(generator);
+  double r = uni_dist(generator);
   // Metropolis test
   if (r <= W.find(Delta_E)->second) {
     spin(rx, ry) *= -1;
@@ -103,96 +91,17 @@ void Metropolis(mat &spin, double T, int L, map<double, double> W, double &E,
 void MC(mat &spin, double T, int L, double &E, double &M, int mcs, int GS,
         int *energies) {
 
-  // initialize(spin, L, E, M, GS);
+  initialize(spin, L, E, M, GS);
 
   // Setting up the RNG with a seed determined from the machine clock
   unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
   mt19937 generator(seed);
-  map<double, double> W = transitions(T);
+
+  map<double, double> W = transitions(T); // Allowed transitions for
+
   for (int cycle = 1; cycle < mcs; cycle++) {
     Metropolis(spin, T, L, W, E, M, generator);
     energies[cycle] = E;
   }
   return;
-}
-
-int main(int argc, char *argv[]) {
-  char *outfilename = argv[1];
-  int initial_temp = atof(argv[2]);
-  int final_temp = atof(argv[3]);
-  int temp_step = atoi(argv[4]);
-  int totcycles = atoi(argv[5]);
-  // int mcs = atoi(argv[5]);
-  int L = atoi(argv[6]);
-  // cout << mcs << endl;
-  char *outfilename2;
-
-  int numprocs, my_rank;
-
-  mat spin = zeros(L, L);
-  double E, M, total_E, total_M;
-  E = 0;
-  M = 0;
-  int GS = 0;
-  double T = initial_temp;
-
-  ofstream outfile;
-
-  initialize(spin, L, E, M, GS);
-  MPI_Init(&argc, &argv);
-  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-  int mcs = totcycles / numprocs;
-  int *energies;
-  int *mag_mom;
-  energies = new int[mcs];
-  mag_mom = new int[mcs];
-  // broadcast to all nodes common variables
-  // MPI_Bcast(&L, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  // MPI_Bcast(&initial_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  // MPI_Bcast(&final_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  // MPI_Bcast(&temp_step, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-  // for (int T = initial_temp; T < final_temp; T += temp_step) {
-
-  MC(spin, T, L, E, M, mcs, GS, energies);
-  if (numprocs == 1) {
-    if (my_rank == 0) {
-
-      MPI_Status status;
-      outfile.open(outfilename, ios::binary);
-      outfile.write(reinterpret_cast<const char *>(energies),
-                    mcs * sizeof(int));
-
-      for (int i = 1; i < numprocs; i++) {
-        MPI_Recv(energies, mcs, MPI_INT, MPI_ANY_SOURCE, 500, MPI_COMM_WORLD,
-                 &status);
-        outfile.write(reinterpret_cast<const char *>(energies),
-                      mcs * sizeof(int));
-      }
-
-      outfile.close();
-    } else {
-      MPI_Send(energies, mcs, MPI_INT, 0, 500, MPI_COMM_WORLD);
-      // MPI_Finalize();
-    }
-  }
-
-  MPI_Reduce(&E, &total_E, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-  MPI_Reduce(&M, &total_M, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-  // if (rank == 0) {
-  outfilename2 = "mean_energy.txt";
-  ofstream meanfile;
-  meanfile.open(outfilename2);
-  meanfile << total_E << " " << total_M;
-  meanfile.close();
-  // }
-  // delete energies;
-  // delete[] energies;
-  // }
-  MPI_Finalize();
-
-  return 0;
 }
--- project4/main.cpp
+++ project4/main.cpp
@@ -1,7 +1,9 @@
+#include "Ising.h"
+
 int main(int argc, char *argv[]) {
   char *outfilename = argv[1];
-  int initial_temp = atoi(argv[2]);
-  int final_temp = atoi(argv[3]);
+  int initial_temp = atof(argv[2]);
+  int final_temp = atof(argv[3]);
   int temp_step = atoi(argv[4]);
   int totcycles = atoi(argv[5]);
   // int mcs = atoi(argv[5]);
@@ -15,62 +17,72 @@ int main(int argc, char *argv[]) {
   double E, M, total_E, total_M;
   E = 0;
   M = 0;
-  int GS = 0;
-  double T = 1.0;
+  int GS = 1;
+  double T = initial_temp;
 
   ofstream outfile;
 
-  initialize(spin, L, E, M, GS);
+  // initialize(spin, L, E, M, GS);
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
   int mcs = totcycles / numprocs;
-  int *averages;
-  averages = new int[mcs];
-
+  int *energies;
+  int *mag_mom;
+  energies = new int[mcs];
+  mag_mom = new int[mcs];
   // broadcast to all nodes common variables
   // MPI_Bcast(&L, 1, MPI_INT, 0, MPI_COMM_WORLD);
   // MPI_Bcast(&initial_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   // MPI_Bcast(&final_temp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   // MPI_Bcast(&temp_step, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+  double Timestart, Timeend, Totaltime;
+  Timestart = MPI_Wtime();
+
   // for (int T = initial_temp; T < final_temp; T += temp_step) {
 
-  MC(spin, T, L, E, M, mcs, GS, averages);
+  MC(spin, T, L, E, M, mcs, GS, energies);
   if (numprocs == 1) {
     if (my_rank == 0) {
 
       MPI_Status status;
       outfile.open(outfilename, ios::binary);
-      outfile.write(reinterpret_cast<const char *>(averages),
+      outfile.write(reinterpret_cast<const char *>(energies),
                     mcs * sizeof(int));
 
       for (int i = 1; i < numprocs; i++) {
-        MPI_Recv(averages, mcs, MPI_INT, MPI_ANY_SOURCE, 500, MPI_COMM_WORLD,
+        MPI_Recv(energies, mcs, MPI_INT, MPI_ANY_SOURCE, 500, MPI_COMM_WORLD,
                  &status);
-        outfile.write(reinterpret_cast<const char *>(averages),
+        outfile.write(reinterpret_cast<const char *>(energies),
                       mcs * sizeof(int));
       }
 
       outfile.close();
     } else {
-      MPI_Send(averages, mcs, MPI_INT, 0, 500, MPI_COMM_WORLD);
-      // MPI_Finalize();
+      MPI_Send(energies, mcs, MPI_INT, 0, 500, MPI_COMM_WORLD);
     }
   }
 
   MPI_Reduce(&E, &total_E, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(&M, &total_M, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
+  Timeend = MPI_Wtime();
+  Totaltime = Timeend - Timestart;
+  if (my_rank == 0) {
+    cout << "Time = " << Totaltime << " on number of processors: " << numprocs
+         << endl;
+  }
   // if (rank == 0) {
   outfilename2 = "mean_energy.txt";
-  ofstream efile;
-  efile.open(outfilename2);
-  efile << total_E;
-  efile.close();
+  ofstream meanfile;
+  meanfile.open(outfilename2);
+  meanfile << total_E << " " << total_M;
+  meanfile.close();
   // }
-  // delete averages;
-  // delete[] averages;
+  // delete energies;
+  // delete[] energies;
   // }
   MPI_Finalize();
 
