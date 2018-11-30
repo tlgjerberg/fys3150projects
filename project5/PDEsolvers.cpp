00d7bbd801f129d78e0b5b297a99f4c0e11ddaf5
tlgjerberg <torgje@lavabit.com>
2018-11-30 15:21:31 +0100

    Added method for setting up initial condition and set up algorithm for the explicit euler

--- project5/PDEsolvers.cpp
+++ project5/PDEsolvers.cpp
@@ -1,15 +1,23 @@
 #include "PDEsolvers.hpp"
 
-PDEsolvers::PDEsolvers(double init1, double bound1, double bound2) {
-  double u_0 = init1;
+PDEsolvers::PDEsolvers(double bound1, double bound2) {
   double b_0 = bound1;
   double b_L = bound2;
 }
 
-void PDEsolvers::Explicit_Euler(int n) {
+void PDEsolvers::init_cond(double init1, int n) {
+  u = new double[n];
+  double u_0 = init1;
+  for (int i = 0; i < n; i++) {
+    u[i] = u_0;
+  }
+}
 
-  for (int i = 1; i < n; i++) {
-    i = 1;
+void PDEsolvers::Explicit_Euler(int n, double *d, double *b, double *upper) {
+  u[0] = b_0;
+  u[n] = b_L;
+  for (int i = n; i > 0; i--) {
+    u[i] = (b[i] - upper[i] * u[i + 1]) / d[i];
   }
 
   return;
--- project5/PDEsolvers.hpp
+++ project5/PDEsolvers.hpp
@@ -11,11 +11,12 @@ private:
   double u_0;
   double b_0;
   double b_L;
-  double *u;
 
 public:
-  PDEsolvers(double init1, double bound1, double bound2);
-  void Explicit_Euler(int n);
+  double *u;
+  PDEsolvers(double bound1, double bound2);
+  void init_cond(double init1, int n);
+  void Explicit_Euler(int n, double *d, double *b, double *upper);
   void Implicit_Euler();
   void Crank_Nicolson();
 };
