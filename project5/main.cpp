9f35d13f6c2cd51770d31007c8786ab0b231235f
tlgjerberg <torgje@lavabit.com>
2018-11-30 15:23:43 +0100

    Testing PDEsolvers and tridiag_solver classes

--- project5/main.cpp
+++ project5/main.cpp
@@ -9,6 +9,14 @@ int main(int argc, char const *argv[]) {
   Tridiag tridiag = Tridiag(2, -1, -1);
 
   tridiag.initialize(n, hh);
-  // Gauss_Seidel();
+
+  tridiag.Gauss_Seidel(n);
+
+  PDEsolvers pde = PDEsolvers(0, 1);
+
+  pde.init_cond(0, n);
+
+  pde.Explicit_Euler(n, tridiag.d, tridiag.b, tridiag.upper);
+
   return 0;
 }
